ce5c50178926f4f164f49da55cb8407ba376808b
smdeox <smdeox@live.com>
2020-09-15 17:58:57 -0400

    Added Hw 4 solution

---
+++ Lecture2Hw4.py
@@ -0,0 +1,21 @@
+#Write a function that takes an integer x that is greater than 1 and returns the largest integer that is smaller than x and evenly divides x.
+
+def largest_factor(x):
+    """Return the largest factor of x that is smaller than x.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factor is 1 since 13 is prime
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
+    factor = x - 1
+    while factor > 0:
+      if x % factor == 0:
+        return factor
+      factor -= 1
+
+print(largest_factor(24))
